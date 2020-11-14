a444da9d852b26001ca524ca12e4cc05eb7345bc
Tom McIntosh <tom_mcintosh@outlook.com>
2020-11-14 23:39:55 +1000

    Refactor version object creation. Add logDie function for raw output, then die()

--- src/setVersion.js
+++ src/setVersion.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import {die} from "./process.js";
+import {die, logDie} from "./process.js";
 import {readPackageJson} from "./readPackageJson.js";
 import {createLocations} from "./createLocations.js";
-import {constants} from "./constants";
 import {getPlatformConstant} from "./getPlatformConstant";
-import {coreReplacer} from "./coreReplacer";
 import {createVersion} from "./createVersion";
 import {mutateFiles} from "./mutateFiles";
 import {createCliFlags} from "./createCliFlags";
@@ -18,16 +16,6 @@ const commandLineFlags = createCliFlags()
 // Create the version object
 const version = createVersion(process.argv[2])
 
-// Validate version strings
-                        version.raw  || die(`Please provide a version number. ${constants.emoji.warning}`)
-version.validRegEx.test(version.raw) || die(`Usage: setVersion <version> [args] ${constants.emoji.warning} `)
-
-// The core string contains to extended semVer symbols. Eg, 1.1.1 format only.
-version.core = version.raw.replace(version.validRegEx, coreReplacer)
-
-// Stripped is the core string without periods
-version.stripped = version.core.split('.').join('')
-
 // Use CLI args to determine the correct platform constant
 const platform = getPlatformConstant(commandLineFlags)
 
@@ -35,7 +23,7 @@ const platform = getPlatformConstant(commandLineFlags)
 const locations = createLocations(version, packageJson, platform)
 
 // If dryRun is set, just print versions
-if (commandLineFlags.dryRun.set) { console.log(version); die('No changes applied.') }
+commandLineFlags.dryRun.set && logDie(version, 'No changes applied.')
 
 // Make the file mutations
 mutateFiles(locations, commandLineFlags, packageJson, version)
